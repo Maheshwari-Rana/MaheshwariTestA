c1a6cadaf0e3915143bc749a302a37cf433c8dc1
Maheshwari-Rana <maheshwarivrana@gmail.com>
2020-10-31 08:51:24 -0400

    Initial commit

---
+++ maheshwariTestA/src/main/java/maheshwari/rana/s301110467/MaheshwariActivity.java
@@ -0,0 +1,20 @@
+package maheshwari.rana.s301110467;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Full Name: Maheshwari Rana
+ * Student ID: 301110467
+ * Section: 002
+ */
+
+public class MaheshwariActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maheshwari);
+    }
+}
---
+++ maheshwariTestA/src/main/java/maheshwari/rana/s301110467/RanaActivity.java
@@ -0,0 +1,20 @@
+package maheshwari.rana.s301110467;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Full Name: Maheshwari Rana
+ * Student ID: 301110467
+ * Section: 002
+ */
+
+public class RanaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rana);
+    }
+}
